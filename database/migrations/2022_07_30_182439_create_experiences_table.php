f61e0d5ea7fb9294bbcdbe72eb00f2b2030d56dd
Manash Chakrobortty <manash@cefalo.com>
2022-07-31 00:54:58 +0600

    Create necessary migrations

---
+++ database/migrations/2022_07_30_182439_create_experiences_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_title');
+            $table->string('company_name');
+            $table->string('company_web_url')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->text('responsibilities')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
